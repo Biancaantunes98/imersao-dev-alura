2820a03280a66ced27352224c8829b721859555e
MÁRCIO PORTELA <bianca.antunes98@gmail.com>
2022-03-08 01:04:35 -0300

    adding initial files (index, script, style)

---
+++ script.js
@@ -0,0 +1,7 @@
+function calcular(){
+    var peso =   document.getElementById("peso").value
+    
+    var resultado = peso * 35;
+    
+    document.getElementById("resultado").value = resultado;
+  }
